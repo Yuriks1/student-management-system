ece77355a05698941add2e03d218aa0d96e436e8
Yuriks1 <89460670+Yuriks1@users.noreply.github.com>
2022-02-27 13:12:55 +0100

    Create NotFoundException

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,7 +1,87 @@
 package se.iths.rest;
 
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import javax.inject.Inject;
+import java.util.List;
+import se.iths.exception.NotFoundException;
+
+
+@Path("students")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class StudentRest {
+    @Inject
+    StudentService studentService;
+
+    @Path("")
+    @POST
+    public Response createStudent(Student student, @Context UriInfo uriInfo) {
+        studentService.createStudent(student);
+        if (student == null) {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+                    .entity("No students were found.").type(MediaType.TEXT_PLAIN_TYPE)
+                    .type(MediaType.APPLICATION_JSON)
+                    .build());
+        }
+        UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder();
+        uriBuilder.path(String.valueOf(student.getId()));
+        return Response.created(uriBuilder.build()).build();
+    }
+
+    @Path("")
+    @GET
+    public Response getAllStudents() {
+        List<Student> foundStudents = studentService.getAllStudents();
+        if (foundStudents == null) {
+
+            throw new NotFoundException("No Students Found");
+        }
+        return Response.ok(foundStudents).build();
+    }
+
+    @Path("{id}")
+    @PUT
+    public Response updateStudent(Student student) {
+
+        studentService.updateStudent(student);
+
+        if (student == null) {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+                    .entity("No students were found.").type(MediaType.TEXT_PLAIN_TYPE)
+                    .type(MediaType.APPLICATION_JSON)
+                    .build());
+        }
+        return Response.ok(student).build();
+    }
 
 
+    @Path("{id}")
+    @DELETE
+    public Response deleteStudent(@PathParam("id") Long id) {
+        Student foundStudent = studentService.findStudentById(id);
+        if (foundStudent == null) {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+                    .entity("Item does not exist.")
+                    .type(MediaType.APPLICATION_JSON)
+                    .build());
+        }
+        studentService.deleteStudent(id);
+        return Response.ok().build();
+    }
+
+    @Path("{id}")
+    @GET
+    public Response getStudentByLastname(@QueryParam("lastName") String lastName) {
+        if (lastName == null) {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+                    .entity("Item does not exist.")
+                    .type(MediaType.APPLICATION_JSON)
+                    .build());
+        }
+        return Response.ok(studentService.getStudentByLastname(lastName)).build();
+    }
 }
